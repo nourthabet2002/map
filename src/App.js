bb846da912dfb4c7ebddcd4cd4b7de88d5ee6e8e
Nour Thabet <nour2002thabet@gmail.com>
2023-09-24 10:36:52 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import Gmap from'./components/Gmap'
+import SimpleMap from './components/Gmap';
+
+function App() {
+  return (
+    <div className="App">
+      <SimpleMap/>
+    </div>
+  );
+}
+
+export default App;
